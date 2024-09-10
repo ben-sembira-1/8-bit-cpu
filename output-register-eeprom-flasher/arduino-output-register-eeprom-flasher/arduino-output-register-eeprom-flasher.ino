fce818a04e9bb257e4dca1364bb477ec8fc7413f
Ben <ben.sembira.dev@gmail.com>
2024-09-10 17:35:08 +0300

    feat: setup pinMode to all output pins

---
+++ output-register-eeprom-flasher/arduino-output-register-eeprom-flasher/arduino-output-register-eeprom-flasher.ino
@@ -0,0 +1,26 @@
+const int ADDRESS_PINS[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+const int IO_PINS[] = {A0, A1, A2, A3, A4, A5, A6, A7};
+const int CHIP_ENABLE_PIN = 11;
+const int OUTPUT_ENABLE_PIN = 12;
+const int WRITE_ENABLE_PIN = 13;
+
+int intArrayLength(int[] array) {
+  return (sizeof(array) / sizeof(int))
+}
+
+void setPinsToModeOutput(int[] pins) {
+  for (int i = 0; i < intArrayLength(pins); i++) {
+    pinMode(pins[i], OUTPUT)
+  }
+}
+
+void setup() {
+  setPinsToModeOutput(ADDRESS_PINS)
+  setPinsToModeOutput(IO_INPUTS)
+  setPinsToModeOutput([CHIP_ENABLE_PIN, OUTPUT_ENABLE_PIN, WRITE_ENABLE_PIN])
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

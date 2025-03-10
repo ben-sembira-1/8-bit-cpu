1bbf06c645a103deff03bc4be175fbeb72b1d505
Ben <ben.sembira.dev@gmail.com>
2025-03-10 20:33:25 +0200

    feat: tests colors

--- upload.sh
+++ upload.sh
@@ -2,7 +2,15 @@
 
 arduino-cli upload -p /dev/ttyACM0 -b arduino:avr:mega OutputRegisterEepromFlasher/OutputRegisterEepromFlasher.ino -v -t
 echo -e "\n----\n"
-cat /dev/ttyACM0 | while IFS= read -r line; do
-  echo -e "$(date +'%H:%M:%S.%3N')\t$line"
+arduino-cli monitor -p /dev/ttyACM0 -c baudrate=9600 | while IFS= read -r line; do
+	formatted="$(date +'%H:%M:%S.%3N')\t$line"
+	if echo "$line" | grep -qi "Test.*failed\."; then
+    	echo -e "\e[31m$formatted\e[0m"  # Red for lines containing "failed"
+	elif echo "$line" | grep -qi "Test.*passed\."; then
+    	echo -e "\e[32m$formatted\e[0m"  # Green for other lines
+	else
+		echo -e "$formatted"
+	fi
+
 done
 

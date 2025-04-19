f65b20b8744fe6e5481969690ea176330ae8276a
Ben Sembira <ben.sembira.1@gmail.com>
2025-04-19 10:02:58 +0000

    fix: cross compatability with mac and ubuntu

--- setup.sh
+++ setup.sh
@@ -2,5 +2,11 @@
 set -euo pipefail
 . .env
 
+sudo apt-get install -y --no-install-recommends curl ca-certificates
+sudo rm -rf /var/lib/apt/lists/*
+
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sudo sudo BINDIR=/usr/local/bin sh
+arduino-cli version
+
 arduino-cli core install ${CORE}
 arduino-cli lib install AUnit
